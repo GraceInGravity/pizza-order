60af7211018ba1d20d3bd02fa46d2babbb514ac0
nuala <nuala.k.gray@gmail.com>
2019-02-01 10:27:48 -0800

    Add files, start creating pizza, order, and customer objects

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+
+function Pizza(name, toppings, price){
+  this.name = name;
+  this.toppings = toppings;
+  this.price = price;
+}
+
+
+function PizzaOrder ()
+
+function Customer (name, phoneNumber, orderNumber){
+  this.name = name;
+  this.phoneNumber = phoneNumber;
+  this.orderNumber = orderNumber;
+}
+
+
+var cheesePizza = new Pizza("Cheese Pizza", ["tomatoeSauce","cheese"], 15);
+
+var hawaiianPizza = new Pizza("Hawaiian Pizza", ["tomatoeSauce","cheese","pineapple","ham"], 20);
+
+var mushroomPizza = new Pizza("Mushroom Pizza", ["tomatoeSauce","cheese", "mushroom"], 18);
